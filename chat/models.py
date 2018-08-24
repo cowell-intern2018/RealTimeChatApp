4b86bbec8c409a752378f9e19ad0e02fd01b75c8
渡辺仁志 <al15110@shibaura-it.ac.jp>
2018-08-24 17:26:11 +0700

    add User model to caht/models.py

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id_user = models.IntegerField()
+    user_name = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.id_user
