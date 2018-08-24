a3b0fe08a5e9b0688ad59acfc52f9064f5b6dbb8
Doppon <zaxpkuraudo7777@gmail.com>
2018-08-24 11:27:48 +0700

    add signup pge

--- RealTimeChatApp/urls.py
+++ RealTimeChatApp/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
