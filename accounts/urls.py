8f2fa4f2ce6c614f590e336ee76bce95020759ed
渡辺仁志 <al15110@shibaura-it.ac.jp>
2018-08-25 23:38:18 +0700

    commit other changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup_account, name='sign_up'),
+    path('signup/', views.signup_account, name='signup'),
 ]
