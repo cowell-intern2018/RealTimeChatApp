f1149f05afe14023714d42ec9d4b530759f61711
Doppon <zaxpkuraudo7777@gmail.com>
2018-08-29 14:05:40 +0700

    add top url

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'chat'
 
 urlpatterns = [
+    path('', views.top, name='top'),
     path('chat/', views.index, name='index'),
     path('chat/<slug:room_name>/', views.room, name='room'),
 ]
